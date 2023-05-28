b93b10ed5e1ea49849227df174540db076b39b26
Lepip <enotrk@mail.ru>
2023-05-29 02:25:33 +0300

    Documentation, renaming and refining.

--- crystall_defect_cv/__init__.py
+++ crystall_defect_cv/__init__.py
@@ -1,5 +1,5 @@
 from .io import open_png, save_png
-from .processing import find_defects_probs, mark_defects, evaluate_efficiency
-from .processing_modules.modules_wrapper import modules_dict as modules
+from .processing import find_defects_probs, mark_defects, mark_all_in_directory
+from .processing_modules.modules_wrapper import modules_dict
 from .processing_modules.config import config
-__all__ = ["open_png", "save_png", "find_defects_probs", "mark_defects", "modules", "evaluate_efficiency"]
+__all__ = ["open_png", "save_png", "find_defects_probs", "mark_defects", "modules_dict", "mark_all_in_directory"]
--- crystall_defect_cv/processing.py
+++ crystall_defect_cv/processing.py
@@ -9,8 +9,8 @@ from .processing_modules import modules_dict
 def find_defects_probs(image: np.ndarray) -> np.ndarray:
     """
     Processes the image and returns a matrix of defect certainty.
-    :param image: Gray image
-    :returns: Matrix of defect certainties
+    :param image: Gray image.
+    :returns: Matrix of defect certainties.
     """
     probability_matrices = {module_name: module(image) for module_name, module in modules_dict.items()}
     output_probs = merge_probability_matrices(probability_matrices)
@@ -20,6 +20,10 @@ def find_defects_probs(image: np.ndarray) -> np.ndarray:
 def mark_defects(image: np.ndarray, defects_matrix: np.ndarray, lighten_image_up=True):
     """
     Marks defects on the image based on the probability matrix.
+    :param image: An image to mark defects on.
+    :param defects_matrix: Matrix of the same size as image, every nonzero element is treated like a defect.
+    :param lighten_image_up: If True, lights up the image by 10.
+    :returns: Marked RGB image with red squares.
     """
     marked_image = image.copy()
     marked_image = cv2.cvtColor(marked_image * (lighten_image_up * 9 + 1), cv2.COLOR_GRAY2RGB)
@@ -27,13 +31,13 @@ def mark_defects(image: np.ndarray, defects_matrix: np.ndarray, lighten_image_up
     for c in contours:
         (x, y, w, h) = cv2.boundingRect(c)
         w = h = max(w, h)
-        cv2.rectangle(marked_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.rectangle(marked_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
     return marked_image
 
 
 def merge_probability_matrices(probability_matrices: {str: np.ndarray}) -> np.ndarray:
     """
-    Merges
+    Merges matrices. Returns element-wise max of them.
     :param probability_matrices: Array of probability matrices produced by different defect detecting techniques.
     :returns: Matrix of merged probability matrices into one.
     """
@@ -47,49 +51,22 @@ def merge_probability_matrices(probability_matrices: {str: np.ndarray}) -> np.nd
     return output_matrix
 
 
-def evaluate_efficiency(params):
+def mark_all_in_directory(operator, directory, save_directory):
+    """
+    Marks all the images in the directory, using the operator as defect-detect technique and saves results
+    in the save_directory.
+    :param save_directory: directory to save marked images to.
+    :param operator: function (img)->img, that marks the defects. For example "sobel_technique" from module_sobel.py.
+    :param directory: directory of .png images with defects.
+    """
     import cv2
     from . import io
-    effectiveness = 0
-    losses = 0
-    ok = 0
-    not_ok_num = 0
-    global_not_ok = 0
-    first = 1
-    num_img = 0
-    images_dirs = os.listdir("./pictures")
-    for image_dir in images_dirs:
-        if image_dir.find("_") != -1:
+    images_dirs = os.listdir(directory)
+    for image_name in images_dirs:
+        if image_name.find("_") != -1:
             continue
-        name = image_dir.split('.')
-        img = io.open_png("./pictures/" + image_dir)
-        img_xy = params["operator"](img)
-        log_img = np.sign(img_xy) * np.log1p(np.abs(img_xy))
-        what = np.abs(log_img) > params["threshold"]
-        blured = cv2.blur(src=what.astype(np.float64) * 255, ksize=(34, 34)) ** params["power"]
-        blured = cv2.normalize(blured, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        blured[blured < np.mean(blured) + 20] = 0
-        contours, hierarchy = cv2.findContours(blured, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        red_image = cv2.imread("./pictures/" + name[0] + "_red.png")
-        not_ok = 0
-        img10 = red_image*10
-        for c in contours:
-            (x, y, w, h) = cv2.boundingRect(c)
-            w = h = max(w, h)
-            if params["show"] == 1:
-                cv2.rectangle(img10, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            red_crop = red_image[x:x+w, y:y+h]
-            mean = np.mean(red_crop, axis=(0, 1))
-            if mean[2] > mean[1]:
-                ok += 1
-            else:
-                not_ok += w*h
-                not_ok_num += 1
-        global_not_ok += not_ok/1500./1500.
-        num_img += 1
-        if params["show"] == 1:
-            cv2.imshow(name[0], img10)
-    global_not_ok /= num_img
-    if params["show"] == 1:
-        cv2.waitKey(0)
-    return ok / params["hm"], not_ok_num / params["hm"], global_not_ok
+        img = io.open_png(directory + "/" + image_name)
+        img_xy = operator(img)
+        img_razm = mark_defects(img, img_xy)
+        cv2.imwrite(save_directory + "/" + image_name, img_razm)
+    cv2.waitKey(0)
